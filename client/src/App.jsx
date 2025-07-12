c40b9506ccfb216675ed5a91626f01064c308be1
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2025-07-12 12:53:19 +0530

    basic routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './componets/Dashboard'
+import LandingPage from './componets/LandingPage'
+import Navbar from './componets/Navbar'
 import Login from './componets/Login'
+import Browse from './componets/Browse'
+import AddItem from './componets/AddItem'
+
 
 function App() {
   return (
    <div>
     {/* define all route here */}
-    <Login></Login>
+     <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/add-item" element={<AddItem />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
    </div>
   )
 }
