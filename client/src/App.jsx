21c23b1a3485657a3246d5874ce2e39084df4681
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2025-07-12 12:26:27 +0530

    login page basic structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './componets/Login'
 
 function App() {
   return (
    <div>
     {/* define all route here */}
-    <h1>App</h1>
+    <Login></Login>
    </div>
   )
 }
