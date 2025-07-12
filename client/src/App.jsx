a6c5af1c14767a103f179c28d1d636bce036f33c
Amankumar-G <amangaloliya@gmail.com>
2025-07-12 12:42:22 +0530

    Tailwind css reconfigured and SchadCN added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
-import Login from './componets/Login'
+// import './App.css'
+// import Login from './componets/Login'
+
+// function App() {
+//   return (
+//    <div>
+//     {/* define all route here */}
+//     <Login></Login>
+//    </div>
+//   )
+// }
+
+// export default App
+
+
+//sample working for testing..
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-   <div>
-    {/* define all route here */}
-    <Login></Login>
-   </div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
